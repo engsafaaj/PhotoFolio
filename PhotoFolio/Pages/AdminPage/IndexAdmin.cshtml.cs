cc0875910b05ec8a85c974711b8540fd138a2543
tecno1001 <utecno76@gmail.com>
2022-09-17 10:39:33 +0300

    Add project files.

---
+++ PhotoFolio/Pages/AdminPage/IndexAdmin.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhotoFolio.Pages.AdminPage
+{
+    public class IndexAdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
